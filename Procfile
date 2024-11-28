0cb45ff50d7e54036e08d90e8d5baa5b0ed52bd7
NejcZivic <n3jczivic@gmail.com>
2024-11-28 14:25:59 +0100

    Rewrite!

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: deno run --allow-net --allow-read --allow-env --allow-run --allow-write server.ts
+web: npm start
--- server.ts
+++ server.ts
@@ -1,12 +1,9 @@
-import express, { Response, Request } from "npm:express@latest";
-import fileupload from "npm:express-fileupload@latest";
-import * as bsmap from 'https://raw.githubusercontent.com/KivalEvan/BeatSaber-Deno/main/mod.ts';
-import { compress, decompress } from "https://deno.land/x/zip@v1.2.5/mod.ts";
-import fromV3Lightshow from "./functions/convertV3.ts";
-import { AudioData } from "https://raw.githubusercontent.com/KivalEvan/BeatSaber-Deno/main/beatmap/v4/audioData.ts";
-
-const app = express();
-app.use(fileupload());
+import multer from "multer";
+import AdmZip from "adm-zip";
+import express, { json, Request } from "express";
+import path from "path";
+import { Info, loadInfo, readAudioDataFileSync, saveInfo, readDifficultyFileSync, readLightshowFileSync, writeDifficultyFileSync, writeAudioDataFile } from "bsmap";
+import fs from "fs";
 
 function removeEmpty(o: any) {
     for (let key in o) {
@@ -22,106 +19,165 @@ function removeEmpty(o: any) {
     return o;
 }
 
-function changeInfo(info: bsmap.v4.Info) {
+function changeInfo(info: Info) {
     info.difficulties.forEach(diff => {
         diff.colorSchemeId = 0;
     });
 }
 
-async function exists(filename: string): Promise<boolean | any> {
+function exists(filename: string, returnPath: boolean = false): boolean | any {
     try {
-        await Deno.stat(filename)
-
-        return true;
-    } catch (err) {
-        if (err instanceof Deno.errors.NotFound) {
-            return false;
-        } else {
-            return err;
+        const stats = fs.statSync(filename);
+        if (stats.isDirectory()) {
+            throw new Error(`Expected a file but found a directory: ${filename}`);
         }
+
+        if (returnPath) {
+            return path.join(filename);
+        }
+
+        const data = fs.readFileSync(filename, 'utf-8');
+        return JSON.parse(data);
+    } catch (err) {
+        console.error(`Error in exists function for file ${filename}:`, err);
+        return false;
     }
 }
+
 
 async function ConvertV4(): Promise<boolean> {
-    const existFile = await exists("map/Info.dat")
-    if (!existFile) return false;
-    const info = await bsmap.load.info(4, { filePath: "map/Info.dat" });
-    changeInfo(info);
-    let audioData: AudioData;
-    if (info.audio.audioDataFilename) {
-        audioData = bsmap.load.audioDataSync(`map/${info.audio.audioDataFilename}`, 4);
-        const BPMInfo = {
-            _version: "2.0.0",
-            _songSampleCount: audioData.sampleCount,
-            _songFrequency: audioData.frequency,
-            _regions: audioData.bpmData.map(d => {
-                return {
-                    _startSampleIndex: d.startSampleIndex,
-                    _endSampleIndex: d.endSampleIndex,
-                    _startBeat: d.startBeat,
-                    _endBeat: d.endBeat
-                }
-            })
-        };
-        Deno.writeTextFileSync(`converted/BPMInfo.dat`, JSON.stringify(BPMInfo));
-    }
-    for (const diff of info.difficulties) {
-        const v4Diff = bsmap.load.difficultySync(`map/${diff.filename}`);
-        const v4Lightshow = bsmap.load.lightshowSync(`map/${diff.lightshowFilename}`);
-        const v3Diff = bsmap.convert.toV3Difficulty(v4Diff);
-        const v3Lightshow = bsmap.convert.toV3Lightshow(v4Lightshow);
+    try {
+        const infoFile = exists(path.join("map", "Info.dat"));
+        if (!infoFile) return false;
+        const info = loadInfo(infoFile, 4) as Info;
+        changeInfo(info);
 
-        const newDiff = fromV3Lightshow(v3Diff, v3Lightshow);
+        const audioFile = exists(path.join("map", info.audio.audioDataFilename), true)
+        const audioData = readAudioDataFileSync(audioFile).setFilename(
+            'BPMInfo.dat',
+        );
+        writeAudioDataFile(audioData, 2);
 
-        // @ts-ignore
-        if (audioData) {
-            const bpmEvents = bsmap.load.audioDataSync(`converted/BPMInfo.dat`).getBpmEvents();
-            bpmEvents.forEach(e => {
-                newDiff.addBpmEvents(e);
-            })
+        const bpmEvents = audioData.getBpmEvents();
+
+        fs.writeFileSync(`converted/BPMInfo.dat`, JSON.stringify(audioData));
+
+        for (const diff of info.difficulties) {
+            const v4DiffFile = exists(path.join("map", diff.filename), true);
+            const v4Diff = readDifficultyFileSync(v4DiffFile);
+            const v4LightshowFile = exists(path.join("map", diff.lightshowFilename), true);
+            const v4Lightshow = readLightshowFileSync(v4LightshowFile);
+            v4Diff.lightshow = v4Lightshow.lightshow;
+
+            if (audioData) {
+                v4Diff.bpmEvents = [];
+                v4Diff.addBpmEvents(...bpmEvents);
+            }
+
+            const newDiff = writeDifficultyFileSync(v4Diff, 3);
+
+            const copy: any = JSON.parse(JSON.stringify(newDiff));
+
+            if (diff.characteristic === "Standard") {
+                fs.writeFileSync(`converted/${diff.difficulty}.beatmap.dat`, JSON.stringify(removeEmpty(copy)));
+            } else {
+                fs.writeFileSync(`converted/${diff.characteristic}${diff.difficulty}.beatmap.dat`, JSON.stringify(removeEmpty(copy)));
+            }
         }
 
-        const copy: any = JSON.parse(JSON.stringify(newDiff));
-
-        if (diff.characteristic === "Standard") {
-            Deno.writeTextFileSync(`converted/${diff.difficulty}.beatmap.dat`, JSON.stringify(removeEmpty(copy)));
-        } else {
-            Deno.writeTextFileSync(`converted/${diff.characteristic}${diff.difficulty}.beatmap.dat`, JSON.stringify(removeEmpty(copy)));
-        }
+        const convertedInfo = saveInfo(info, 2);
+        const infoCopy = JSON.parse(JSON.stringify(convertedInfo));
+        fs.writeFileSync(`converted/Info.dat`, JSON.stringify(infoCopy));
+        fs.writeFileSync(`converted/${info.audio.filename}`, fs.readFileSync(`map/${info.audio.filename}`));
+        info.coverImageFilename != "" ? fs.writeFileSync(`converted/${info.coverImageFilename}`, fs.readFileSync(`map/${info.coverImageFilename}`)) : null;
+        return true;
+    } catch (e) {
+        console.log(e)
+        return false;
     }
-    const convertedInfo = bsmap.convert.toV2Info(info);
-    Deno.writeTextFileSync(`converted/Info.dat`, JSON.stringify(convertedInfo));
-    Deno.writeFileSync(`converted/${info.audio.filename}`, Deno.readFileSync(`map/${info.audio.filename}`));
-    info.coverImageFilename != "" ? Deno.writeFileSync(`converted/${info.coverImageFilename}`, Deno.readFileSync(`map/${info.coverImageFilename}`)) : null;
-    return true;
 }
 
-app.get("/", (_req: Request, res: Response) => {
-    const page = Deno.readTextFileSync("index.html")
-    res.send(page)
+const storage = multer.diskStorage({
+    destination: (req, res, cb) => {
+        cb(null, "./processed/");
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
 });
 
-app.post("/convert", async (req: Request, res: Response) => {
-    const data = req.files.fileUploaded.data
-    Deno.writeFileSync("map.zip", new Uint8Array(data))
+const upload = multer({ storage: storage, limits: { fieldSize: 20 * 1024 * 1024 } });
 
-    await decompress("map.zip", "map");
+export const uploadMiddleware = upload.fields([
+    { name: "map", maxCount: 1 },
+]);
+
+interface MulterFile {
+    map?: Express.Multer.File[];
+}
+
+// @ts-ignore
+interface TypedRequest extends Request {
+    files?: MulterFile;
+}
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ limit: "30mb", extended: true, parameterLimit: 10000 }));
+
+app.get("/", (req, res) => {
+    const html = String(fs.readFileSync(path.join(__dirname, "index.html")));
+    res.send(html);
+})
+
+// @ts-ignore
+app.post("/api/upload", uploadMiddleware, async (req: TypedRequest, res) => {
+    const mapZip = req.files?.map ? (req.files?.map as Express.Multer.File[])[0] : undefined
+
+    if (!mapZip) {
+        return res.status(400).send("No map provided.");
+    }
+
+    fs.mkdirSync(path.join(__dirname, "map"), { recursive: true });
+    fs.mkdirSync(path.join(__dirname, "converted"), { recursive: true });
+
+    const convertedName = mapZip.filename.split(".")[0]
+
+    const zipPath = path.join(mapZip.destination, mapZip.filename);
+
+    const zip = new AdmZip(zipPath);
+    zip.extractAllTo(path.join(__dirname, "map"));
 
     const converted = await ConvertV4();
 
-    if (!converted) return res.send("Failure.\nZip structure incorrect.")
-    await compress("converted", "convertedMap")
-    res.download("convertedMap.zip");
-    setTimeout(() => {
-        Deno.removeSync("map", { recursive: true });
-        Deno.mkdirSync("map");
-        Deno.removeSync("converted", { recursive: true });
-        Deno.mkdirSync("converted");
-        Deno.removeSync("map.zip");
-        Deno.removeSync("convertedMap.zip");
-    }, 2000)
+    if (converted) {
+
+        const newZip = new AdmZip();
+
+        newZip.addLocalFolder(path.join(__dirname, "converted"))
+
+        const buffer = newZip.toBuffer();
+
+        res.set({
+            'Content-Type': 'application/zip',
+            'Content-Disposition': `attachment; filename="${convertedName}_converted.zip"`,
+            'Content-Length': buffer.length
+        });
+
+        res.send(buffer);
+
+        res.on('finish', () => {
+            fs.unlinkSync(zipPath);
+            fs.rmSync(path.join(__dirname, "map"), { recursive: true, force: true });
+            fs.rmSync(path.join(__dirname, "converted"), { recursive: true, force: true });
+        });
+    } else {
+        fs.unlinkSync(zipPath);
+        fs.rmSync(path.join(__dirname, "map"), { recursive: true, force: true });
+        fs.rmSync(path.join(__dirname, "converted"), { recursive: true, force: true });
+        return res.status(500).send("Something went wrong.")
+    }
 });
 
-const port = Deno.env.get("PORT") || 3000
-
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(3000, () => console.log("Listening on port 3000"));
